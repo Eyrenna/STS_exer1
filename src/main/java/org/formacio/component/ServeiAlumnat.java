0dc4c216917ce9b005aed05f56e4804c1480f478
Claudia <claudia365x@gmail.com>
2019-04-14 19:23:00 +0200

    Configurada Inyección de dependencias

--- src/main/java/org/formacio/component/ServeiAlumnat.java
+++ src/main/java/org/formacio/component/ServeiAlumnat.java
@@ -1,10 +1,13 @@
 package org.formacio.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServeiAlumnat {
 
+	@Autowired
+	private RepositoriAlumnes repo;
 	
 	/**
 	 * ha de donar d'alta a la base de dades d'alumnes l'alumne indicat amb 
@@ -13,6 +16,12 @@ public class ServeiAlumnat {
 	 * Retorna true si l'alumne s'ha inserit, false si no.
 	 */
 	public boolean matricula (int id, String alumne) {
+		if (alumne == null) {
+			return false;
+		}
+		else {
+			repo.altaAlumne(id, alumne);
+		}
 		return true;
 	}
 	
